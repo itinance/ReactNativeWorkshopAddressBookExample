d3e3f304855ab4a87553096b886d1029f203af0d
Hagen Hübel <hhuebel@itinance.com>
2018-01-30 10:28:03 +0100

    Added navigation/routing library

--- App.js
+++ App.js
@@ -12,11 +12,26 @@ import {
   View
 } from 'react-native';
 
+import {
+  Scene,
+  Router,
+  Actions,
+  ActionConst,
+  Stack,
+} from 'react-native-router-flux';
+
+import AddressListComponent from './src/components/AddressBook/AddressListComponent'
+import AddressItemComponent from './src/components/AddressBook/AddressItemComponent'
+
 export default class App extends Component<{}> {
   render() {
     return (
-      <View style={styles.container}>
-      </View>
+      <Router>
+          <Stack key="root">
+            <Scene key="main" component={AddressListComponent} title="Addressbook"/>
+            <Scene key="register" component={AddressItemComponent} title="Address"/>
+          </Stack>
+      </Router>      
     );
   }
 }
--- src/components/AddressBook/AddressListComponent.js
+++ src/components/AddressBook/AddressListComponent.js
@@ -26,6 +26,6 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    backgroundColor: 'lightgray',
   },
 });
