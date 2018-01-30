514135f7f120b2be909ad8d99f51b8061fdcee35
Hagen Hübel <hhuebel@itinance.com>
2018-01-30 10:11:36 +0100

    Initial commit

---
+++ src/components/AddressBook/AddressItemComponent.js
@@ -0,0 +1,31 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+
+export default class AddressItemComponent extends Component<{}> {
+  render() {
+    return (
+      <View style={styles.container}>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+});
